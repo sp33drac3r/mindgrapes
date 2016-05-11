8d90fffb790ad91aa00671775d70737ef490f596
Riley Scheid <riley.k.scheid@gmail.com>
2016-05-11 09:09:00 -0700

    Add skeleton for rest client and paragraph parsing

--- app/helpers/text_analysis_helper.rb
+++ app/helpers/text_analysis_helper.rb
@@ -1,8 +1,27 @@
 helpers do
   def analyze_post(post)
+    cleaned_post = post.gsub(/"/,"'")
+    cleaned_post.split("\n")
+    cleaned_post.each do | paragraph |
+      Paragraph.create()
+    end
+
     #break post into paragraphs with regex
     #send each post to text analysis
     #store in sentence database
     #return hash with pos nut and neg figures
   end
+
+  def gather_sentiment(paragraph)
+    sentiment = RestClient.post("http://text-processing.com/api/sentiment/", text: paragraph)
+  end
 end
+
+
+
+a = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.
+
+  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
+
+b = a.split("\n")
+p b
