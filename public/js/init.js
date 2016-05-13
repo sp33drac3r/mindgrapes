b15d56f1e97f8c157ba4a6b830d6754ef99c9d53
Riley Scheid <riley.k.scheid@gmail.com>
2016-05-13 10:07:23 -0700

    Add semantic UI

--- public/js/init.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('select').material_select();
-  Materialize.updateTextFields();
-  $('input#input_text, textarea#textarea1').characterCounter();
-  $('#textarea1').val('New Text');
-  $('#textarea1').trigger('autoresize');
-});
