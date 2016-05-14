efeb48533af84a5dd5a26351ed5ea57a0cfb7f7a
Riley Scheid <riley.kenyon@gmail.com>
2016-05-14 11:05:56 -0700

    Add session helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,19 @@
+use Rack::Session::Pool, :expire_after => 2592000
+
 helpers do
   def current_user
-    user ||= User.find(session(:user_id).to_i)
+    @user ||= User.find_by(id: session[:user_id].to_i)
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout!
+    session[:user_id] = nil
   end
 end
